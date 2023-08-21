2622736454f28f8abb0b18fddd49edc67c8c43fc
racchu1234 <racchu1234@gmail.com>
2023-08-21 16:45:43 +0530

    change8

---
+++ python8.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np 
+
+data={"racchu": [18],"acchu":[18],"paru":[18],"nagu":[18]}
+df = pd.DataFrame(data)
+print(df)
+
+print()
+
+data1={"name":['acchu','paru','nagu'],"age":[18,18,18]}
+df1 = pd.DataFrame(data1)
+print(df1)
